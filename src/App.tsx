accc0bdad2262f0abd280adb5dd2fb0df84e95b6
ksandoval <ksandoval@aseguate.com>
2022-07-08 09:37:55 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "@wcj/dark-mode";
+import "./App.css";
+import MDEditor from "@uiw/react-md-editor";
+// import MDStr from "./README.md";
 
-function App() {
+const Editor = () => {
+  const [value, setValue] = useState<string | undefined>("");
+  return (
+    <>
+      <div className="App-editor">
+        <MDEditor value={value} onChange={setValue} />
+        <MDEditor.Markdown source={value} style={{ whiteSpace: "pre-wrap" }} />
+      </div>
+    </>
+  );
+};
+
+const Documentation = () => {
+  return (
+    <>
+      <p>working...</p>
+    </>
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <dark-mode permanent light="Light" dark="Dark"></dark-mode>
+      <Routes>
+        <Route path="/" element={<Editor />} />
+        <Route path="doc" element={<Documentation />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
