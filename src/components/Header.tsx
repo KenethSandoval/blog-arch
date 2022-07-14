e1af3f5fe4b6d85c622292f6dd60d758cb9f5130
Стив <kenethra.74@gmail.com>
2022-07-13 21:39:59 -0600

    feat(frontend): dark mode and context mdx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,40 @@
 import Link from "next/link";
+import { useTheme } from "next-themes";
+import { SunIcon, MoonIcon } from "@heroicons/react/solid";
+import { useEffect, useState } from "react";
 
 const Header: React.FC = () => {
+	const { systemTheme, theme, setTheme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+			setMounted(true);
+	}, []);
+
+	const renderThemeChanger = () => {
+		if(!mounted) return null;
+
+		const currentTheme = theme === "system" ? systemTheme : theme;
+
+		if(currentTheme === "dark") {
+				return (
+					<SunIcon className="w-10 h-10 text-yellow-500" role="button" onClick={() => setTheme("light")} />
+				)
+		} else {
+			return (
+			    <MoonIcon className="w-10 h-10 text-gray-900" role="button" onClick={() => setTheme("dark")} />
+			)
+		}
+	};
+
 	return (
-	  <header className="py-2">
-		  <Link href="/">
-				<a className="text-2xl font-bold text-green-500">Gestor de documentacion tecnica</a>
-			</Link>
+	  <header className="h-15 shadow-sm dark:border-gray-700"> 
+			<div className="container py-4 flex justify-between items-center">
+				<Link href="/">
+					<a className="text-2xl font-bold text-green-500">Gestor de documentacion tecnica</a>
+				</Link>
+			  {renderThemeChanger()}
+			</div>
 		</header>
 	);
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import 'tailwindcss/tailwind.css';
+import { ThemeProvider } from "next-themes"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
+			<ThemeProvider enableSystem={true} attribute="class">
 		    <Layout>
 			    <Component {...pageProps} />
 			  </Layout>
+			</ThemeProvider>
 		);
 }
