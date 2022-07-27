d23a5e1cc4a22f0379a216e4dbed7c7a8476ed6e
Стив <54087279+KenethSandoval@users.noreply.github.com>
2022-07-26 22:33:50 -0600

    Merge dev (#9)
    * build/workflow
    - workflow for github
    
    * build/workflow
    - change action to actions
    
    * perf(backend): change backend struct
    
    * ci(backend): check workflow
    
    * ci(backend): remove clear in makefile
    
    * ci(backend): on only PR
    
    * feat(backend): add server with echo
    
    * feat(backend): update gitignore
    
    * ci(backend): workflow for testing
    
    * ci(backend): repair sintax yml
    
    * style(fronted): change card list documentation (#7)
    
    * feat(backend): connection mongodb
    
    * test(backend): test server (#8)
    
    * test(backend): test server
    
    * ci(backend): update test.yml
    
    * ci(backend): remove mongo version
    
    * Update test.yml

--- backend/internal/infrastructure/app/server_test.go
+++ backend/internal/infrastructure/app/server_test.go
@@ -1,1 +1,18 @@
 package app_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/KenethSandoval/doc-md/internal/infrastructure/tester"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHTTPErrorNotFound(t *testing.T) {
+	testy := tester.New()
+
+	code, body := testy.RequestWithServe(http.MethodGet, "/xxx", nil, nil)
+
+	assert.Equal(t, http.StatusNotFound, code)
+	assert.Equal(t, "{\"message\":\"Not Found\"}\n", body)
+}
