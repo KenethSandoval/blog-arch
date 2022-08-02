3863e1bc607991002a2a1aa383aa3822784b412b
Стив <kenethra.74@gmail.com>
2022-08-01 18:52:40 -0600

    feat: add read data for client

---
+++ backend/cmd/cli/config/config.go
@@ -0,0 +1,111 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+const AppDir = "."
+
+const SendFileName = "data.yml"
+
+type DataSend struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+type ConfigParser struct{}
+
+type Data struct {
+	Body DataSend `yaml:"data"`
+}
+
+func (parser ConfigParser) getDefaultData() Data {
+	return Data{
+		DataSend{
+			Username: "",
+			Password: "",
+		},
+	}
+}
+
+func (parser ConfigParser) getDefaultDataYamlContent() string {
+	defaultConfig := parser.getDefaultData()
+	yaml, _ := yaml.Marshal(defaultConfig)
+
+	return string(yaml)
+}
+
+func (parser ConfigParser) writeDefaultConfigContents(newConfigFile *os.File) error {
+	_, err := newConfigFile.WriteString(parser.getDefaultDataYamlContent())
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (parser ConfigParser) createSendFileIfMissing(sendFilePath string) error {
+	if _, err := os.Stat(sendFilePath); errors.Is(err, os.ErrNotExist) {
+		newSendFile, err := os.OpenFile(sendFilePath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+		if err != nil {
+			return err
+		}
+
+		defer newSendFile.Close()
+		return parser.writeDefaultConfigContents(newSendFile)
+	}
+	return nil
+}
+
+func (parser ConfigParser) getConfigFileOrCreateIfMissing() (*string, error) {
+	var err error
+
+	sendFilePath := filepath.Join(AppDir, SendFileName)
+	err = parser.createSendFileIfMissing(sendFilePath)
+
+	if err != nil {
+		return nil, err
+	}
+	return &sendFilePath, nil
+}
+
+func (parser ConfigParser) readSendFile(path string) (Data, error) {
+	dataConfig := parser.getDefaultData()
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return dataConfig, err
+	}
+
+	err = yaml.Unmarshal((data), &dataConfig)
+
+	return dataConfig, err
+}
+
+func initParser() ConfigParser {
+	return ConfigParser{}
+}
+
+func ParserConfig() (Data, error) {
+	var data Data
+	var err error
+
+	parser := initParser()
+
+	configFilePath, err := parser.getConfigFileOrCreateIfMissing()
+	if err != nil {
+		return data, err
+	}
+
+	config, err := parser.readSendFile(*configFilePath)
+
+	if err != nil {
+		return data, err
+	}
+
+	return config, nil
+}
