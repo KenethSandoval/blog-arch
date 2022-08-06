273d39e451aedd0e284475bf0df8950fd2946913
Стив <kenethra.74@gmail.com>
2022-08-05 22:19:21 -0600

    feat(frontend): add guard

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,18 +1,42 @@
-import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
 import 'tailwindcss/tailwind.css';
+import type { AppProps } from 'next/app';
 import { ThemeProvider } from "next-themes"
+import Router from 'next/router';
+import { NextPage } from 'next';
+import { AuthGuard } from "src/guard/AuthGuard";
+
+import Layout from '../components/Layout';
 import { MdxComponentsProvider } from '../context/mdxContext';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+
+/*Router.events.on("routeChangeStart", () => {
+	console.log("start");
+});*/
+
+export type NextApplicationPage<P = {}, IP = P> = NextPage<P, IP> & {
+		auth?: boolean;
+};
+
+export default function MyApp(props: AppProps) {
+		const { Component, pageProps } : { Component: NextApplicationPage; pageProps: any } = props;
     return (
-			<ThemeProvider enableSystem={true} attribute="class">
-				<MdxComponentsProvider>
-				  <Layout>
-			    	<Component {...pageProps} />
-			  	</Layout>
-				</MdxComponentsProvider>
-		    
-			</ThemeProvider>
+			<>
+				{Component.auth ? (
+						<AuthGuard>
+							<ThemeProvider enableSystem={true} attribute="class">
+								<MdxComponentsProvider>
+				  				<Layout>
+			    					<Component {...pageProps} />
+			  					</Layout>
+								</MdxComponentsProvider>
+							</ThemeProvider>
+						</AuthGuard>
+					) : (
+						<>
+							<Component {...pageProps} />
+						</>
+					)
+				}	
+			</>
 		);
 }
