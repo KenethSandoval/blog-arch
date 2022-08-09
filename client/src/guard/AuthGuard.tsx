81cf59c1e72e09afe8c39d99c38d97abaed0c7b8
Стив <kenethra.74@gmail.com>
2022-08-08 21:27:40 -0600

    feat(frontend): edit guard

--- client/src/guard/AuthGuard.tsx
+++ client/src/guard/AuthGuard.tsx
@@ -1,39 +1,40 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-// import { useAuth } from "@global-store/useAuth"
-import { useAuth } from "./AuthProvider";
+import { setRedirect, useAuth } from "@global-store/useAuth"
+// import { useAuth } from "./AuthProvider";
 
 interface Props {
 	children: React.ReactNode;
 };
 
-export function AuthGuard({ children }: { children: JSX.Element }) {
-  const { user, initializing, setRedirect } = useAuth()
-  const router = useRouter()
+export const AuthGuard: React.FC<Props> = ({ children }: Props) => {
+	const { checking } = useAuth((state) => state);
+	const router = useRouter();
 
-  useEffect(() => {
-    if (!initializing) {
-      //auth is initialized and there is no user
-      if (!user) {
-        // remember the page that user tried to access
-        setRedirect(router.route)
-        // redirect
-        router.push("/login")
-      }
-    }
-  }, [initializing, router, user, setRedirect])
+	useEffect(() => {
+		if (!checking) {
+				setRedirect(router.route);
+				router.push("/login");
+			}
+	}, [checking, router]);
 
-  /* show loading indicator while the auth provider is still initializing */
-  if (initializing) {
-    return <h1>Application Loading</h1>
+	/* show loading indicator while the auth provider is still isLoading */
+  if (checking) {
+    return (
+      <div className="absolute h-screen w-screen z-50">
+        <div className="flex overflow-hidden fixed top-0 right-0 bottom-0 left-0 z-50 flex-col justify-center items-center w-full h-screen bg-opacity-80 bg-gray-300">
+          <div className="mb-4 w-12 h-12 rounded-full border-4 border-t-4 border-gray-200 ease-linear loader animate-spinner" />
+          <h2 className="text-xl font-semibold text-center text-primary-base">
+            Loading...
+          </h2>
+        </div>
+      </div>
+    );
   }
 
-  // if auth initialized with a valid user show protected page
-  if (!initializing && user) {
-    return <>{children}</>
-  }
+	if (!checking) {
+		return <>{children}</>;
+	}
 
-  /* otherwise don't return anything, will do a redirect from useEffect */
-  return null
+	return null;
 }
-
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,7 +2,6 @@ import 'tailwindcss/tailwind.css';
 import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from "next-themes"
-import Router from 'next/router';
 import { NextPage } from 'next';
 
 import NProgress from "nprogress"
@@ -10,7 +9,6 @@ import NProgress from "nprogress"
 import Layout from '../components/Layout';
 import { MdxComponentsProvider } from '../context/mdxContext';
 import { AuthGuard } from "../guard/AuthGuard";
-import { AuthProvider } from "../guard/AuthProvider";
 
 /*Router.events.on("routeChangeStart", () => {
 	NProgress.start();
@@ -27,7 +25,6 @@ export default function MyApp(props: AppProps) {
 				<ThemeProvider enableSystem={true} attribute="class">
 					<MdxComponentsProvider>
 				  	<Layout>
-						<AuthProvider>
 							{Component.auth ? (
 								<AuthGuard>
 			    				<Component {...pageProps} />
@@ -36,7 +33,6 @@ export default function MyApp(props: AppProps) {
 			    				<Component {...pageProps} />
 							)
 							}
-						</AuthProvider>
 			  		</Layout>
 					</MdxComponentsProvider>
 				</ThemeProvider>
